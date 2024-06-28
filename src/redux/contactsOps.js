21b81b3eb4896b42cff57909f82c2712ddec951a
FatchenkoSofiia <fatcenkosona@gmail.com>
2024-06-28 16:14:19 +0300

    required

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://66670073a2f8516ff7a5dfd7.mockapi.io";
